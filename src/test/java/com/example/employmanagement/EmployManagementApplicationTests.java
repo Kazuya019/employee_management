26cfb749c180328dd077110421a7859c3302ef4b
Kazuya Nakashima <kazuya.nakashima@sjsu.edu>
2022-03-10 17:04:23 -0800

    add framework

---
+++ src/test/java/com/example/employmanagement/EmployManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
