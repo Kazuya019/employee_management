1b2fea68d21a68c0bd23bf54b5554f9d0783d606
Anna Li <shuna.li@sjsu.edu>
2022-08-24 18:57:08 -0700

    testing1

--- src/main/ui/src/App.js
+++ src/main/ui/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello </h1>
+        <h1>Hello0000000 </h1>
       </header>
     </div>
   );
