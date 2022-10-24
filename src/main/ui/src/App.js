c022da85ccbae83cd4d3634c46f4abccd682cbcd
Xiran Jia <87832775+xiranabby@users.noreply.github.com>
2022-10-23 19:08:59 -0700

    Update App.js

--- src/main/ui/src/App.js
+++ src/main/ui/src/App.js
@@ -2,14 +2,13 @@ import "./App.css";
 import "./index.css";
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
 import RegisterScreen from "./Screens/RegisterScreen";
 import HomeScreen from "./Screens/HomeScreen";
 import UserMainScreen from "./Screens/UserMainScreen";
 import TaskScreen from "./Screens/TaskScreen";
 import SigninScreen from "./Screens/SignInScreen";
 import UserMessageScreen from "./Screens/UserMessageScreen";
+import CalendarScreen from "./Screens/CalendarScreen";
 
 function App() {
   return (
@@ -20,6 +19,7 @@ function App() {
       <Route path="/main" component={UserMainScreen}></Route>
       <Route path="/message" component={UserMessageScreen}></Route>
       <Route path="/task" component={TaskScreen}></Route>
+      <Route path="/calendar" component={CalendarScreen}></Route>
     </BrowserRouter>
   );
 }
